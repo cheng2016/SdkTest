6e9fb8864b453f57b14adc5d318547e5435183c7
mitnick <1102743539@qq.com>
2019-07-12 12:47:43 +0800

    Update FileUtils.java

--- library/src/main/java/com/icloud/sdk/utils/FileUtils.java
+++ library/src/main/java/com/icloud/sdk/utils/FileUtils.java
@@ -11,8 +11,12 @@ import com.alibaba.fastjson.JSON;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 public class FileUtils {
   public static String PACK_NAME = "icloud";
