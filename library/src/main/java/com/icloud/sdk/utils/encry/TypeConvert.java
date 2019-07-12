7ccacb37423524d6c576a7425d182c104f5968d4
mitnick <1102743539@qq.com>
2019-07-12 09:58:26 +0800

    Update TypeConvert.java

--- library/src/main/java/com/icloud/sdk/utils/encry/TypeConvert.java
+++ library/src/main/java/com/icloud/sdk/utils/encry/TypeConvert.java
@@ -98,136 +98,32 @@ public class TypeConvert {
   
   public byte[] short2byte(int paramInt) { return new byte[] { (byte)(paramInt >> 8), (byte)paramInt }; }
   
-  public String toHexString(byte[] paramArrayOfByte, int paramInt1, int paramInt2) { // Byte code:
-    //   0: new java/lang/StringBuffer
-    //   3: dup
-    //   4: invokespecial <init> : ()V
-    //   7: astore #7
-    //   9: iconst_0
-    //   10: istore #4
-    //   12: iload_2
-    //   13: istore #5
-    //   15: iload #4
-    //   17: istore #6
-    //   19: iload #5
-    //   21: iload_3
-    //   22: iload_2
-    //   23: iadd
-    //   24: if_icmpge -> 45
-    //   27: iload #4
-    //   29: iconst_1
-    //   30: iadd
-    //   31: istore #4
-    //   33: iload #4
-    //   35: getstatic com/icloud/sdk/utils/encry/TypeConvert.MAX_HEX_LENGTH : I
-    //   38: if_icmplt -> 83
-    //   41: iload #4
-    //   43: istore #6
-    //   45: new java/lang/StringBuilder
-    //   48: dup
-    //   49: invokespecial <init> : ()V
-    //   52: aload #7
-    //   54: invokevirtual toString : ()Ljava/lang/String;
-    //   57: invokevirtual append : (Ljava/lang/String;)Ljava/lang/StringBuilder;
-    //   60: astore #7
-    //   62: iload #6
-    //   64: getstatic com/icloud/sdk/utils/encry/TypeConvert.MAX_HEX_LENGTH : I
-    //   67: if_icmplt -> 105
-    //   70: ldc '...'
-    //   72: astore_1
-    //   73: aload #7
-    //   75: aload_1
-    //   76: invokevirtual append : (Ljava/lang/String;)Ljava/lang/StringBuilder;
-    //   79: invokevirtual toString : ()Ljava/lang/String;
-    //   82: areturn
-    //   83: aload #7
-    //   85: aload_1
-    //   86: iload #5
-    //   88: baload
-    //   89: invokestatic toHexString : (B)Ljava/lang/String;
-    //   92: invokevirtual append : (Ljava/lang/String;)Ljava/lang/StringBuffer;
-    //   95: pop
-    //   96: iload #5
-    //   98: iconst_1
-    //   99: iadd
-    //   100: istore #5
-    //   102: goto -> 15
-    //   105: ldc ''
-    //   107: astore_1
-    //   108: goto -> 73 }
-  
-  public String toHexString2(byte[] paramArrayOfByte, int paramInt1, int paramInt2) { // Byte code:
-    //   0: new java/lang/StringBuffer
-    //   3: dup
-    //   4: invokespecial <init> : ()V
-    //   7: astore #7
-    //   9: new java/util/Formatter
-    //   12: dup
-    //   13: aload #7
-    //   15: invokespecial <init> : (Ljava/lang/Appendable;)V
-    //   18: astore #8
-    //   20: iconst_0
-    //   21: istore #4
-    //   23: iload_2
-    //   24: istore #5
-    //   26: iload #4
-    //   28: istore #6
-    //   30: iload #5
-    //   32: iload_3
-    //   33: iload_2
-    //   34: iadd
-    //   35: if_icmpge -> 56
-    //   38: iload #4
-    //   40: iconst_1
-    //   41: iadd
-    //   42: istore #4
-    //   44: iload #4
-    //   46: getstatic com/icloud/sdk/utils/encry/TypeConvert.MAX_HEX_LENGTH : I
-    //   49: if_icmplt -> 94
-    //   52: iload #4
-    //   54: istore #6
-    //   56: new java/lang/StringBuilder
-    //   59: dup
-    //   60: invokespecial <init> : ()V
-    //   63: aload #7
-    //   65: invokevirtual toString : ()Ljava/lang/String;
-    //   68: invokevirtual append : (Ljava/lang/String;)Ljava/lang/StringBuilder;
-    //   71: astore #7
-    //   73: iload #6
-    //   75: getstatic com/icloud/sdk/utils/encry/TypeConvert.MAX_HEX_LENGTH : I
-    //   78: if_icmplt -> 125
-    //   81: ldc '...'
-    //   83: astore_1
-    //   84: aload #7
-    //   86: aload_1
-    //   87: invokevirtual append : (Ljava/lang/String;)Ljava/lang/StringBuilder;
-    //   90: invokevirtual toString : ()Ljava/lang/String;
-    //   93: areturn
-    //   94: aload #8
-    //   96: ldc '%02x'
-    //   98: iconst_1
-    //   99: anewarray java/lang/Object
-    //   102: dup
-    //   103: iconst_0
-    //   104: aload_1
-    //   105: iload #5
-    //   107: baload
-    //   108: invokestatic valueOf : (B)Ljava/lang/Byte;
-    //   111: aastore
-    //   112: invokevirtual format : (Ljava/lang/String;[Ljava/lang/Object;)Ljava/util/Formatter;
-    //   115: pop
-    //   116: iload #5
-    //   118: iconst_1
-    //   119: iadd
-    //   120: istore #5
-    //   122: goto -> 26
-    //   125: ldc ''
-    //   127: astore_1
-    //   128: goto -> 84 }
+  public String toHexString(byte[] b, int offset, int length) {
+    StringBuffer buffer = new StringBuffer();
+    length += offset;
+    int max = 0;
+    for (int i = offset; i < length; ++i) {
+      max++;
+      if (max >= MAX_HEX_LENGTH) {
+        break;
+      }
+      buffer.append(toHexString(b[i]));
+    }
+    return buffer.toString() + (max >= MAX_HEX_LENGTH ? "..." : "");
+  }
+
+  public String toHexString2(byte[] b, int offset, int length) {
+    StringBuffer buffer = new StringBuffer();
+    Formatter formatter = new Formatter(buffer);
+    length += offset;
+    int max = 0;
+    for (int i = offset; i < length; ++i) {
+      max++;
+      if (max >= MAX_HEX_LENGTH) {
+        break;
+      }
+      formatter.format("%02x", b[i]);
+    }
+    return buffer.toString() + (max >= MAX_HEX_LENGTH ? "..." : "");
+  }
 }
-
-
-/* Location:              C:\Users\mitni\Desktop\gitwork\AndroidTool\classes-dex2jar.jar!\com\icloud\sd\\utils\encry\TypeConvert.class
- * Java compiler version: 6 (50.0)
- * JD-Core Version:       1.0.6
- */
