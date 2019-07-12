b9e602c41eed4b64cfc9dd24c980486724517e92
mitnick <1102743539@qq.com>
2019-07-12 10:20:08 +0800

    Update ResultCode.java

--- library/src/main/java/com/icloud/sdk/listener/ResultCode.java
+++ library/src/main/java/com/icloud/sdk/listener/ResultCode.java
@@ -1,17 +1,7 @@
 package com.icloud.sdk.listener;
 
-public static enum ResultCode {
-  CANCEL, Fail, SUCCESS;
-  
-  static  {
-    CANCEL = new ResultCode("CANCEL", true);
-    Fail = new ResultCode("Fail", 2);
-    $VALUES = new ResultCode[] { SUCCESS, CANCEL, Fail };
-  }
+public enum  ResultCode {
+    SUCCESS,
+    CANCEL,
+    Fail,
 }
-
-
-/* Location:              C:\Users\mitni\Desktop\gitwork\AndroidTool\classes-dex2jar.jar!\com\icloud\sdk\listener\ResultCode.class
- * Java compiler version: 6 (50.0)
- * JD-Core Version:       1.0.6
- */
