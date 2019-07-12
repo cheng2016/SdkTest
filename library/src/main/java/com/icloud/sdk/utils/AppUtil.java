18b3b66d4cce4b530523748c426fbe2c40eba397
mitnick <1102743539@qq.com>
2019-07-12 12:46:26 +0800

    Update AppUtil.java

--- library/src/main/java/com/icloud/sdk/utils/AppUtil.java
+++ library/src/main/java/com/icloud/sdk/utils/AppUtil.java
@@ -3,6 +3,7 @@ package com.icloud.sdk.utils;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.*;
 
 public class AppUtil {
     public static int getVersionCode(Context context) {
